53a50d5994cb8b5ad15433d86bcd6a68df345d4f
Mahindra Gajjala <mahindra7396@gmail.com>
2025-04-15 18:03:27 +0530

    pointer

--- pointer/main.go
+++ pointer/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
-	referencedereference "pointer/reference_dereference"
 )
 
-func main() {
-	var data int
-	var data_pointer = &data
-	fmt.Println(*data_pointer)
+// func main() {
+// 	var data int
+// 	var data_pointer = &data
+// 	fmt.Println(*data_pointer)
 
-	referencedereference.Reference()
+//		referencedereference.Reference()
+//	}
+func main() {
+	var a int = 10
+	var p *int = &a // Reference: store address of 'a' into pointer 'p'
+	b := &a
+
+	//cannot use &a (value of type *int) as int value in variable declarationcompilerIncompatibleAssign
+
+	fmt.Println("Address of a:", p)
+	fmt.Println(b)
 }
